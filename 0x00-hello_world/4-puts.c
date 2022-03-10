30b58a0349f3ff298a9d53a85b6b2261d9d70b6a
idrissdjio <djiofackidriss.gmail.com>
2022-03-10 05:54:45 -0800

    Added task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * here we want to print a line
+ * we will be using the puts
+ * we should return 0 always
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
