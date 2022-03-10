d68a5deea2f980d2a38310e193d624fd67a98902
idrissdjio <djiofackidriss.gmail.com>
2022-03-10 06:47:53 -0800

    Added the advanced 101-quote.c tak

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	
+	return (1);
+}
