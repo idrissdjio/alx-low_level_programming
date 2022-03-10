55f474a2f8df88cacd6ace6982670e775e015e30
idrissdjio <djiofackidriss.gmail.com>
2022-03-10 06:52:21 -0800

    Added the 101-quote.c task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -8,7 +9,7 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	
 	return (1);
 }
