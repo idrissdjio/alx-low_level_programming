bab54642b7c1a71851bbabee4cab1ebed6cbd6e2
idrissdjio <djiofackidriss.gmail.com>
2022-03-11 08:33:03 -0800

    Added task7&8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,9 +7,13 @@
 
 int main(void)
 {
+	int num;
 	char letter;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
 
 	putchar('\n');
