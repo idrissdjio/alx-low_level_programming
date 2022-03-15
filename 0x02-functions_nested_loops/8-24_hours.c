924448505f5b0dcd1f6bdbb58556e4d1d23ecd86
idrissdjio <djiofackidriss.gmail.com>
2022-03-15 08:51:29 -0700

    added task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		for(minute = 0; minute <= 59; minute++)
+		for (minute = 0; minute <= 59; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
