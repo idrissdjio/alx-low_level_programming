13622fe094e583648f5b398010cdc6beea120f8c
idrissdjio <djiofackidriss.gmail.com>
2022-03-15 08:30:35 -0700

    added task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,5 +8,12 @@
 
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0')'
+
+	return (last_digit);
 }
