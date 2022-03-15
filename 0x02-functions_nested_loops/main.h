745a1764f82514c0e74fc88b84cb00c9e98d908a
idrissdjio <djiofackidriss.gmail.com>
2022-03-15 07:21:54 -0700

    added task0 and main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,12 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef main_h
+#define main_h
+
+/**
+ * File: main.h
+ * Auth: Idriss Djiofack
+ * Desc: Header file containing declarations for
+ * 	all functions of our projects
+ */
 
 int _putchar(char c);
 
