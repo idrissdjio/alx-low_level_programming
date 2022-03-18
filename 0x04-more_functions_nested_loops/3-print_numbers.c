d73ecd04e1c295121f0ba0eb57688493bfeef835
idrissdjio <djiofackidriss.gmail.com>
2022-03-18 07:10:46 -0700

    added task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,9 +10,9 @@ void print_numbers(void)
 {
 	int number;
 
-	for (number = '0'; number < '10'; number++)
+	for (number = 0; number < 10; number++)
 	{
-		_putchar(number);
+		_putchar(number + '0');
 	}
 	_putchar('\n');
 }
