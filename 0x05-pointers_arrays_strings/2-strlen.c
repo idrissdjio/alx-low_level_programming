dc172c4e7745f1f234831fadc6d9464516c4a36e
idrissdjio <djiofackidriss.gmail.com>
2022-03-21 12:03:03 -0700

    added task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ *
+ * @*s: value to be evaluated
+ *
+ * Return - returns the length
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
