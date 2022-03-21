9c10daba43780196e4e8fd7db33c1d64fd1f81d7
idrissdjio <djiofackidriss.gmail.com>
2022-03-21 12:21:56 -0700

    added task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,18 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strlen - return the length of a string
  *
- * @*s: value to be evaluated
+ * @s: value to be evaluated
  *
- * Return - returns the length
+ * Return: returns the length
  */
 
 int _strlen(char *s)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 	while (s[i])
 		i++;
 
