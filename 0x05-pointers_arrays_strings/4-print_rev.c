1732e717c40f1be259debd6cc5709b886e01967b
idrissdjio <djiofackidriss.gmail.com>
2022-03-21 13:01:13 -0700

    added task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse the string
+ *
+ * @s: to be evaluated
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int counter;
+
+	i = 0;
+	while (s[i])
+		i++;
+
+	for (counter = i-1; count >= 0; counter--)
+		_putchar(s[counter]);
+
+	_putchar('\n');
+}
+
