0333e594f96703a7f020b933c5deec3746a91b0c
idrissdjio <djiofackidriss.gmail.com>
2022-03-30 05:47:31 -0700

    added task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,7 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*s)
-	{
-		len++;
-		_strlen_recursion(++s);
-	}
-
-	return (len);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
