39a53d4f2adfac800d893c8c69769a03009075fc
idrissdjio <djiofackidriss.gmail.com>
2022-04-11 11:47:59 -0700

    added task0 and 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,6 +6,7 @@
  * @name: name of the dog
  * @age: age of dog
  * @owner: owner
+ *
  */
 
 struct dog
@@ -16,14 +17,13 @@ struct dog
 };
 
 /**
- * dog_t - Typeof the dog
- *
+ * dog_t - Typedef for dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
