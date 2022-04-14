da0df59b6bc2ac4ee995f1a5c50d81920c6a85e1
idrissdjio <djiofackidriss.gmail.com>
2022-04-14 09:15:34 -0700

    added all tasks of projects 0x10-variadic_functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers and newline
+ * @separator: string 
+ * @n: number of int
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list params;
+
+	va_start(params, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(params, int));
+		if (separator && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
