bfc3738879c282e1f3a1444b4d940250d20aa3c6
Idriss <57317425+idrissdjio@users.noreply.github.com>
2022-05-03 00:55:13 +0800

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints to the POSIX
- * @filename: name of the file
- * @letters: number of characters
- * Return: number of char read/written
+ * read_textfile - reads a file and writes it to stdout
+ * @filename: name of the file to read
+ * @letters: number of characters to read/write
+ * Return: number of characters read/written
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
